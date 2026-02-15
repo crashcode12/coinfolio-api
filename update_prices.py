47a1100e28084c162c97c6a88edd0c8f78303840
crashcode12 <ishayau@gmail.com>
2026-02-16 01:59:08 +0200

    Update update_prices.py

--- update_prices.py
+++ update_prices.py
@@ -129,6 +129,9 @@ def get_data():
 <html lang="he" dir="rtl">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta http-equiv="Expires" content="0">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
         body {{ margin: 0; font-family: Arial, sans-serif; background: transparent; }}
